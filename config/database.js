ffd9604b10838e714a0651eca38bc8bed4d33e90
Renee Rosquites <renee.rosquites@gmail.com>
2017-10-03 11:09:15 -0700

    ok

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+    //database: 'mongodb://brad:brad@ds121190.mlab.com:21190/meanauthapp',   //prod
+    database: 'mongodb://rrosq:197280rsvR@ds149874.mlab.com:49874/meanapp',    //dev
+    secret: 'yoursecret'
+  }
+  
---
+++ models/user.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+// User Schema
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  taskOne: {
+    type: Boolean,
+    default: false
+  },
+  taskTwo: {
+    type: Boolean,
+    default: false
+  },
+  taskThree: {
+    type: Boolean,
+    default: false
+  }
+});
+
+
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function(id, callback){
+  User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+  const query = {username: username}
+  User.findOne(query, callback);
+}
+
+module.exports.addUser = function(newUser, callback){
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if(err) throw err;
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+    if(err) throw err;
+    callback(null, isMatch);
+  });
+}
